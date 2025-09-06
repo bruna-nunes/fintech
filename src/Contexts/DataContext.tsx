57dd7aaff880697de51e18e4ab1d1e7f2b32fe06
bruna-nunes <brugnunes@gmail.com>
2025-09-05 22:16:47 -0300

    feat: add reactive date from context

--- src/Contexts/DataContext.tsx
+++ src/Contexts/DataContext.tsx
@@ -15,15 +15,29 @@ type IDataContext = {
     data: IVenda[] | null;
     loading: boolean;
     error: string | null;
+    inicio: string;
+    setInicio: React.Dispatch<React.SetStateAction<string>>;
+    final: string;
+    setFinal: React.Dispatch<React.SetStateAction<string>>;
 }
 
+function getDate(daysAgo: number) {
+    const date = new Date();
+    date.setDate(date.getDate() - daysAgo);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
+}
 const DataContext = React.createContext<IDataContext | null>(null);
 
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
-    const { data, loading, error } = useFetch<IVenda[]>('https://data.origamid.dev/vendas/');
+    const [inicio, setInicio] = React.useState(getDate(15));
+    const [final, setFinal] = React.useState(getDate(0));
+    const { data, loading, error } = useFetch<IVenda[]>(`https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`);
 
     return (
-        <DataContext.Provider value={{ data, loading, error }}>
+        <DataContext.Provider value={{ data, loading, error, inicio, setInicio, final, setFinal }}>
             {children}
         </DataContext.Provider>
     );
