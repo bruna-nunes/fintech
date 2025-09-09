0604612f919a735d05f4144f85cd2b84f02d0fbe
bruna-nunes <brugnunes@gmail.com>
2025-09-08 21:25:31 -0300

    feat: adding sell informations in resume page

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
 import { useDataContext } from '../Contexts/DataContext';
 
 function Resumo() {
     const { data } = useDataContext();
-    console.log(data);
+
+    if(data === null) return null;
+
     return (
-        <div>Resumo</div>
+        <section>
+            <div className='resumo flex mb'>
+                <div className='box'>
+                    <h2>Vendas</h2>
+                    {data
+                        .filter((venda) => venda.status !== 'falha')
+                        .reduce((acc, venda) => acc + venda.preco, 0)
+                        .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                </div>
+                <div className='box'>
+                    <h2>Recebido</h2>
+                    {data
+                        .filter((venda) => venda.status === 'pago')
+                        .reduce((acc, venda) => acc + venda.preco, 0)
+                        .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                </div>
+                <div className='box'>
+                    <h2>Processando</h2>
+                    {data
+                        .filter((venda) => venda.status === 'processando')
+                        .reduce((acc, venda) => acc + venda.preco, 0)
+                        .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                </div>
+            </div>
+            <div className='box'>Gráficos</div>
+        </section>
     )
 }
 
