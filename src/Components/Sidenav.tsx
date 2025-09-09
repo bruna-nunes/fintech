f7ab0be359139932e1ed58b376a35260392a866b
bruna-nunes <brugnunes@gmail.com>
2025-09-08 21:59:35 -0300

    feat: add sidenav and links/icons/items

--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import FintechSVG from '../assets/FintechSVG';
+import resumo from '../assets/icons/resumo.svg';
+import vendas from '../assets/icons/vendas.svg';
 
 function Sidenav() {
   return (
-    <div>Sidenav</div>
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech Logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
